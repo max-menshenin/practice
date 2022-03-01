532be98ab444f4dd159d643d988b8707071ecb96
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-01 17:29:50 +0300

    init

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
-
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<h1>test</h1>
 <?php 
  $p = 'Приветствую Вас на моей страничке!';
 ?>
