6942bfad0cf509922156a7dd568b46a2af07006f
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-02 21:15:19 +0300

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+    <?php
+    $p = 'Приветствую Вас на моей страничке!';
+    ?>
+    
+    <?php
+    $name = 'Максим';
+    $surname = 'Меньшенин';
+    $patronomyc = 'Андреевич';
+    $city = 'Москва';
+    $age = 30;
+    ?>
+
+
+    <?php
+    include 'main.php';
+    ?>
+</body>
+
+</html>
