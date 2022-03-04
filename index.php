4891e57077c2ffd99611ed247ee586aa1d22f82e
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-04 16:48:40 +0300

    fonts fixed

--- index.php
+++ index.php
@@ -6,12 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
 </head>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap');
-</style>
-<body>
     <?php
     $p = 'Приветствую Вас на моей страничке!';
     ?>
