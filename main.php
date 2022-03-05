417810c2a89212c1dd548e848532852204f4a009
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-05 12:39:45 +0300

    added knowledje

--- main.php
+++ main.php
@@ -1,78 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Practice </title>
-    <!-- <link rel="stylesheet" href="style.css" /> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-</head>
-<body>
-    
-    <div class="flex-container">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Practice </title>
+        <!-- <link rel="stylesheet" href="style.css" /> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    </head>
+    <body>
 
-        <div class="header">     
-               <?php include 'logo.inc.php' ?>         
-               <?php include 'menu.inc.php' ?>	   
-        </div>       
-     
-        <div class="about_me">
-         
-          <h1>  <?php  echo $p  ?> </h1>
+        <div class="flex-container">
 
-            <div class="data">
-                <div class="myImg">
+            <div class="header">     
+                <?php include 'logo.inc.php' ?>         
+                <?php include 'menu.inc.php' ?>	   
+            </div>       
+
+            <div class="about_me">
+
+                <h1>  <?php echo $p ?> </h1>
+
+                <div class="data">
+                    <div class="myImg">
+                        <?php
+                        echo '<img src="img/php.jpg">';
+                        ?>
+                    </div>
+
+                    <div class="fullname">
+                        <p> Меня зовут 
+                            <?php echo $surname, ' ', $name, ' ', $patronomyc . '<br>';
+                            echo 'город', ' ', $city;
+                            ?>                                      
+                        </p> 
+
+                        <p> Мне
+<?php echo $age; ?>          
+                            лет </p>
+                        <p> Мы научились создавать переменные </p>
+                        <p> Изучили простые операции с ними </p>
+                    </div>
+                </div>
+
+                <div class="knowledge">
+
+                    <?php include 'knowledge.inc.php'; ?>
                     <?php
-                    echo '<img src="img/php.jpg">';
-                    ?>
-            </div>
+                    echo $layout, ' ', $php, ' ', $javascript;
+                    ?> <br>
 
-                <div class="fullname">
-                    <p> Меня зовут 
-                    <?php echo $surname, ' ', $name, ' ', $patronomyc. '<br>'; 
-                          echo 'город', ' ', $city; ?>                                      
-                    </p> 
-           
-                    <p> Мне
-                    <?php  echo $age;   ?>          
-                    лет </p>
-                    <p> Мы научились создавать переменные </p>
-                    <p> Изучили простые операции с ними </p>
+                    <?php
+                    $layout = 10;
+                    $php = 30;
+                    $javascript = $layout + $php;
+                    echo $javascript;
+                    ?>   <br>                
+                    <?php
+                    echo $d;
+                    ?> 
+
+                </div>
+
+                <div class="article">
+                    <p class="text">
+                        Со школьных лет любил программирование, участвовал в городских олимпиадах города Тюмени, входил в тройку призеров, правда писал Pascal, но везде принцип схож по-моему опыту, разве в функциональных языках чуть иначе. Поступил и окончил МИРЭА факультет информационных технологий.
+                        Python начал изучать на последних курсах института.
+Полюбил юникс и линукс, не боюсь консоли, пробовал разные дистрибутивы, как Debian, Ubuntu, Fedora, но сейчас на MacOS.
+                    </p>
                 </div>
             </div>
 
-            <div class="knowledge">
-                                   
-                    <?php  include 'knowledge.inc.php'; ?>
-                    <?php
-                    echo $a, ' ', $b, ' ', $c;
-                    ?> <br>
-                    
-                    <?php
-                        $a = 10;
-                        $b = 20;
-                        $c = $a + $b;
-                        echo $c;
-                    ?>   <br>                
-                     <?php
-                        echo $d;
-                    ?> 
+<?php include 'footer.inc.php' ?>
 
-            </div>
-
-            <div class="article">
-                <p class="text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Recusandae asperiores ducimus dolore explicabo. Animi est amet quibusdam molestias! 
-                    Minus laudantium sapiente dignissimos possimus natus cumque delectus sed, accusantium totam quia?
-                </p>
-            </div>
         </div>
 
-            <?php include 'footer.inc.php' ?>
 
-    </div>
-
-
-</body>
+    </body>
 </html>
