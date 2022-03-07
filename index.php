6871801d4aaeabe3c5d879052e9e0f9bad5062f4
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-07 14:55:18 +0300

    init

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My page</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
 
 </head>
     <?php
     $p = 'Приветствую Вас на моей страничке!';
     ?>
-    <!--  -->
+    
     <?php
     $name = 'Максим';
     $surname = 'Меньшенин';
