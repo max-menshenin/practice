7778b73e7d516150b80946dfadb6fdf73680b776
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-07 19:19:25 +0300

    some knowledge

--- knowledge.inc.php
+++ knowledge.inc.php
@@ -9,7 +9,17 @@
 
 
 <?php
-$layout = 'Верстка удовлетворительна'.'<br>';
+$layout = 'Верстка удовлетворительна, но пока не идеальна'.'<br>';
 $php = 'Создавал проекты на Laravel'.'<br>';
 $javascript = 'Понимаю Javascript'; 
+
+$boolean = True; // присвоить $boolean значение TRUE
+echo $boolean.'<br>';
+$integer = 123;
+echo $integer.'<br>'.'integer — целые числа'.'<br>';
+$double = 1.234;
+echo $double.'- дробные числа'.'<br>';
+$string = 'test';
+echo $string.'<br>';
+
 ?>
